62f5e37987d3786309eb869dc1beda12f962412b
Harphies <tafeez.tech@gmail.com>
2020-02-10 15:14:33 +0100

    myboilerplate

---
+++ standard-boilerplate/src/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable quotes */
+// make bluebird defalut promise
+// eslint-disable-next-line no-global-assign
+Promise = require("bluebird");
+const { port, env } = require("./config/vars");
+const logger = require("./config/logger");
+const app = require("./config/express");
+
+// listen to request
+app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
+
+/**
+ * Exports express
+ * @public
+ */
+
+module.exports = app;
