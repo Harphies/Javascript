f54c9d888b94326b4f052ca9a78f48507bc7698b
Harphies <tafeez.tech@gmail.com>
2020-05-28 15:13:48 +0100

    coonection fixed

--- APIs/NodeJs/REST API/src/app.js
+++ APIs/NodeJs/REST API/src/app.js
@@ -12,8 +12,16 @@ app.use(bodyParser.json());
 app.use("/", router);
 
 // Connect to database
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-  console.log(`connected to the DB`);
-});
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 
 app.listen(4000, () => console.log(`App ruunig on port 4000`));
