c25656cf1c544cf3467a20e730f69e3d61c635d0
Harphies <tafeez.tech@gmail.com>
2020-05-28 14:28:32 +0100

    rest with mongodb

--- APIs/NodeJs/REST API/src/app.js
+++ APIs/NodeJs/REST API/src/app.js
@@ -11,13 +11,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/", router);
 // Connect to database
-mongoose.connect(process.env.DB_CONNECT, () => {
-  console.log("connected to the DB");
-});
-/*
+
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
   console.log(`connected to the DB`);
 });
-*/
 
 app.listen(4000, () => console.log(`App ruunig on port 4000`));
