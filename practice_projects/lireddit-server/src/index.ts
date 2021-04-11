af422b58e13a2bc78a4a51435002b5debb436732
Harphies <tafeez.tech@gmail.com>
2021-04-11 18:18:40 +0100

    database setup

---
+++ practice_projects/lireddit-server/src/index.ts
@@ -0,0 +1,19 @@
+import {MikroORM} from "@mikro-orm/core"
+import { __prod__ } from "./constants"
+import { Post } from "./entities/Post"
+import microConfig from "./mikro-orm.config"
+import express from "express"
+
+
+const main = async() => {
+    const orm = await MikroORM.init(microConfig) 
+    await orm.getMigrator().up() 
+
+    const app = express()
+    app.listen(4000, () => {
+        console.log('server started')
+    })
+}
+
+main()
+.catch(err => console.error(err))
