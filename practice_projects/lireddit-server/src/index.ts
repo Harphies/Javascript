427ad6a1a7c8e4e3a0fceae9c7657a10e8836c1c
Harphies <tafeez.tech@gmail.com>
2021-04-12 14:17:56 +0100

    setup graphql server with typescript

--- practice_projects/lireddit-server/src/index.ts
+++ practice_projects/lireddit-server/src/index.ts
@@ -3,6 +3,9 @@ import { __prod__ } from "./constants"
 import { Post } from "./entities/Post"
 import microConfig from "./mikro-orm.config"
 import express from "express"
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello"
 
 
 const main = async() => {
@@ -10,6 +13,16 @@ const main = async() => {
     await orm.getMigrator().up() 
 
     const app = express()
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers:[HelloResolver],
+            validate: false
+        })
+    })
+
+    apolloServer.applyMiddleware({ app })
+
     app.listen(4000, () => {
         console.log('server started')
     })
