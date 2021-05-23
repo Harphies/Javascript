c49e53e3957e8ab862be71114acc3a347f52c3eb
Harphies <tafeez.tech@gmail.com>
2021-05-23 18:43:05 +0100

    start all over

--- practice_projects/gympoint/app.js
+++
@@ -1,8 +0,0 @@
-import './src/utils/checkEnv'
-
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import * as Sentry from '@sentry/node'
-import Youch from 'youch'
-import 'express-async-errors'
--- practice_projects/gympoint/src/config/redisConfig.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv'.config()
-import redis from 'redis'
-
-const Redisclient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
-})
-
-export default Redisclient
