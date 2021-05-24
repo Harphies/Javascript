079c10d0f0ce9029cde76bfc24ab2e2d98d05cbf
user <user@war00308s-mbp.home>
2021-05-24 12:39:51 +0100

    src folder deleted to be created

--- practice_projects/gatsby-linkedin-resume-generator/src/utils/path.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export const ROOT_FOLDER = process.cwd();
-
-export const RESUME_PATH = path.join(ROOT_FOLDER, 'resume.json');
-export const LINKED_IN_PATH = path.join(ROOT_FOLDER, 'linked-in.json');
